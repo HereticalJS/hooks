fd50a05c885484658d80fadee0916602d2f767f4
caasi Huang <caasi.igd@gmail.com>
2019-12-02 19:36:09 +0800

    Cleanup

--- cypress/integration/use-maybe.js
+++ cypress/integration/use-maybe.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { fixScope } from '../support/fix-react-dom-scope';
+import { useMaybe, useOptional } from '../../';
 import {
   WithUndefined,
   WithoutUndefined,
 } from '../components/Maybe';
 
 beforeEach(() => fixScope(ReactDOM)(window));
+
+describe('`useOptional`', () => {
+  it('should be an alias of `useMaybe`', () => {
+    expect(useOptional).to.equal(useMaybe);
+  });
+});
 
 describe('`useMaybe`', () => {
   it('should not trigger the continuation', () => {
